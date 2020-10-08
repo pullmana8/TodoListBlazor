e87171954e98dc3b8e03ff138cddb7179e6fa344
Antonette Caldwell <pullmana8@gmail.com>
2020-10-07 19:48:01 -0500

    Changed cluster to k8s

--- deployments/terraform/k8s.tf
+++ deployments/terraform/k8s.tf
@@ -9,11 +9,11 @@ terraform {
 
 provider "helm" {
   kubernetes {
-    host = azurerm_kubernetes_cluster.cluster.kube_config[0].host
+    host = azurerm_kubernetes_cluster.k8s.kube_config[0].host
 
-    client_key             = base64decode(azurerm_kubernetes_cluster.cluster.kube_config[0].client_key)
-    client_certificate     = base64decode(azurerm_kubernetes_cluster.cluster.kube_config[0].client_certificate)
-    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.cluster.kube_config[0].cluster_ca_certificate)
+    client_key             = base64decode(azurerm_kubernetes_cluster.k8s.kube_config[0].client_key)
+    client_certificate     = base64decode(azurerm_kubernetes_cluster.k8s.kube_config[0].client_certificate)
+    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.k8s.kube_config[0].cluster_ca_certificate)
     load_config_file       = false
   }
 }
